a5ba7c1b66e11aa673438a60cd5ed638921e24e3
David Eves <deves@pobox.com>
2012-02-17 19:48:21 -0800

    configure timeclock admin

---
+++ timeclock/admin.py
@@ -0,0 +1,6 @@
+from timeclock.models import ClockPunch, Worker, Activity
+from django.contrib import admin
+ 
+admin.site.register(ClockPunch)
+admin.site.register(Worker)
+admin.site.register(Activity)
