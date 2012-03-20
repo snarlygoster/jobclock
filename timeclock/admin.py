dfa8bab477c5c39af11fe27bbd69a14182b6f83c
David Eves <deves@pobox.com>
2012-03-20 15:43:04 -0700

    make Activity/job list editable in admin

--- timeclock/admin.py
+++ timeclock/admin.py
@@ -1,6 +1,17 @@
 from timeclock.models import ClockPunch, Worker, Activity
 from django.contrib import admin
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    #date_hierarchy = '<#...#>'
+    list_display = ('ticket', 'description', 'on_work_queue', 'job_complete')
+    list_editable = ('on_work_queue', 'job_complete') 
+    list_filter = ('on_work_queue', 'job_complete') 
+    #save_as = True
+    save_on_top = True
+    #inlines = [<#...#>]
+ 
  
 admin.site.register(ClockPunch)
 admin.site.register(Worker)
-admin.site.register(Activity)
+admin.site.register(Activity, ActivityAdmin)
