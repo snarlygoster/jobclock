579b0b50412f0bd10971b22071f1c969768d142a
David Eves <deves@pobox.com>
2012-05-10 14:31:01 -0700

    basic admin to browse workperiods

--- timeclock/admin.py
+++ timeclock/admin.py
@@ -1,25 +1,30 @@
-from timeclock.models import ClockPunch, Worker, Activity
+from timeclock.models import ClockPunch, Worker, Activity, WorkPeriod
 from django.contrib import admin
 
+admin.site.register(ClockPunch)
 
 class ActivityAdmin(admin.ModelAdmin):
     #date_hierarchy = '<#...#>'
     list_display = ('ticket', 'description', 'on_work_queue', 'job_complete')
-    list_editable = ('on_work_queue', 'job_complete') 
-    list_filter = ('on_work_queue', 'job_complete') 
+    list_editable = ('on_work_queue', 'job_complete')
+    list_filter = ('on_work_queue', 'job_complete')
     #save_as = True
     save_on_top = True
     #inlines = [<#...#>]
 
+admin.site.register(Activity, ActivityAdmin)
 
 class WorkerAdmin(admin.ModelAdmin):
     list_display = ('name', 'can_work')
     list_editable = ( 'can_work',)
     list_filter = ('can_work',)
-     
+
     save_as = True
     save_on_top = True
- 
-admin.site.register(Worker, WorkerAdmin) 
-admin.site.register(ClockPunch)
-admin.site.register(Activity, ActivityAdmin)
+
+admin.site.register(Worker, WorkerAdmin)
+
+class WorkPeriodAdmin(admin.ModelAdmin):
+    list_display = ('worker','start_time','duration','job')
+
+admin.site.register(WorkPeriod, WorkPeriodAdmin)
