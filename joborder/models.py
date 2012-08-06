520b659bc0879ac67c1076ff1692d28ffbf7de12
David Eves <deves@pobox.com>
2012-08-06 07:37:10 -0700

    remodel of job order to use dynamic q&a for item specs

--- joborder/models.py
+++ joborder/models.py
@@ -8,7 +8,8 @@ class Product(models.Model):
     """Type of binding or object thing"""
 
     name = models.CharField(_('name'), max_length=50, blank=False, null=False)
-
+    details = models.CharField(_('details'), max_length=1000, blank=True, null=True, help_text="questions to answer for this type of product")
+    
     class Meta:
         ordering = ['name',]
         verbose_name, verbose_name_plural = "product", "products"
@@ -25,10 +26,9 @@ class Customer(models.Model):
     """Customer Contact Info for a JobOrder"""
     name = models.CharField(_('name'), max_length=100, blank=True, null=True)
     email = models.EmailField()
-    slug = models.SlugField()
 
     class Meta:
-        ordering = ['name',]
+        ordering = ['name', 'email']
 
     def __unicode__(self):
         return "%s" % (self.name)
@@ -37,39 +37,39 @@ class Customer(models.Model):
     def get_absolute_url(self):
         return ('')
 
-class JobOrder(models.Model):
-    """Job information for preparing estimate and shop work"""
 
-    product = models.ForeignKey(Product)
-    quantity = models.PositiveIntegerField(blank=True, null=True)
-    duedate = models.DateField(_('Due Date'), blank=True, null=True)
-    covermaterial = models.CharField(_('Cover Material'), max_length=100, blank=True, null=True )
+class Job(models.Model):
+    """Job connects customer to jobitems and scheduled activities"""
+    
+    customer = models.ForeignKey(Customer) 
+    creation_time = models.DateTimeField(_('created'), auto_now_add=True)
+    
 
     class Meta:
-        #ordering = ['id',]
-        verbose_name, verbose_name_plural = "Job order", "Job orders"
-
+        ordering = ['customer',]
+        verbose_name, verbose_name_plural = "job", "jobs"
+ 
     def __unicode__(self):
-        return "%s" % (self.id)
-
+        return "%s" % (self.customer.name)
+ 
     @models.permalink
     def get_absolute_url(self):
         return ('')
 
-
-class ScheduledJob(models.Model):
-    """ready to be put on the clock"""
-
-    joborder = models.ForeignKey(JobOrder)
-    customer = models.ForeignKey(Customer)
-
-    creation_date = models.DateTimeField(_('creation date'), blank=True, null=True, auto_now_add=True)
-
+class JobItem(models.Model):
+    """Job information for preparing estimate and shop work"""
+    
+    job = models.ForeignKey(Job)
+    product = models.ForeignKey(Product)
+    quantity = models.PositiveIntegerField(blank=True, null=True)
+    detail_answers = models.CharField(_('details'), max_length=1000, blank=True, null=True, help_text="")
+    
     class Meta:
-        ordering = ['creation_date',]
+        #ordering = ['id',]
+        verbose_name, verbose_name_plural = "Job item", "Job Items"
 
     def __unicode__(self):
-        return "%s" % (self.ticket)
+        return "%s" % (self.id)
 
     @models.permalink
     def get_absolute_url(self):
